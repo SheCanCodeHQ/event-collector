e3fb246ce5ebef4387a5a961037f78b69b6049cb
Link Arneson <linkarneson@gmail.com>
2017-08-02 01:40:13 -0700

    added google sheets integration

--- eventbrite_collector.py
+++ eventbrite_collector.py
@@ -1,13 +1,18 @@
 import keys # CONTAINS API KEYS
 import requests
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+#for gspread
+SCOPES = ["https://spreadsheets.google.com/feeds"]
 
 women_keywords = ["women"]#, "woman", "girl"]
 tech_keywords = ["tech"]#, "technology", "code", "programming", "programmer"]
+csv_titles = "name, description, host, start_time, end_time, event_type, location, city, country, price, link, tags"
 
 ids = set()
-lines = []
+lines = [csv_titles]
+rows = [csv_titles.split(', ')]
 
-csv_titles = "name, description, host, start_time, end_time, event_type, location, city, country, price, link, tags"
 
 for i in women_keywords:
     for j in tech_keywords:
@@ -21,10 +26,9 @@ for i in women_keywords:
             verify = True,  # Verify SSL certificate
         )
 
-        # make sure status is live
-
-        for event in [event for event in response.json()['events'] if event['id'] not in ids and event['venue']]:
-            ids.add(event['id'])
+        for event in [event for event in response.json()['events'] if event['id'] not in ids
+                    and event['venue'] and event['status']=='live']:
+            ids.add(event['id']) #TODO: only do updates
 
             name = event['name']['text']
             description = ''#TODO: clean this event['description']['text'] if event['description'] else ''
@@ -39,11 +43,27 @@ for i in women_keywords:
             link = event['url']
             tags = event['category']['name'] if event['category'] else ''
 
-            line = [name, description, host, start_time, end_time, event_type, location, city, country, price, link, tags]
-            line = [str(item).replace(',', ' ') for item in line]
+            details = [name, description, host, start_time, end_time, event_type, location, city, country, price, link, tags]
+
+            #CSV formatting
+            line = [str(item).replace(',', ' ') for item in details]
             line = ", ".join(line)
             lines.append(line)
 
+            #sheets formatting
+            rows.append(details)
+
+#CSV
 with open("events.csv", 'w') as f:
-    f.write(csv_titles + '\n')
     f.write('\n'.join(lines))
+
+#sheets
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(keys.GOOGLE_SHEETS, SCOPES)
+connection = gspread.authorize(credentials)
+
+worksheet = connection.open("Test spreadsheet").sheet1
+
+for row_i in range(len(rows)):
+    for cell_i in range(len(rows[0])):
+        if rows[row_i][cell_i]:
+            worksheet.update_cell(row_i + 1, cell_i + 1, str(rows[row_i][cell_i]))
