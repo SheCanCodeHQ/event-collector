b4944f2c156827861ad7a56b879ff40da5359f6d
Link Arneson <linkarneson@gmail.com>
2017-08-10 12:32:04 -0700

    refine search

--- eventbrite_collector.py
+++ eventbrite_collector.py
@@ -1,37 +1,51 @@
+#python3
 import keys # CONTAINS API KEYS
 import requests
 import gspread
+import datetime
 from oauth2client.service_account import ServiceAccountCredentials
-#for gspread
+
+#TODO: readme for config file
+
 SCOPES = ["https://spreadsheets.google.com/feeds"]
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(keys.GOOGLE_SHEETS, SCOPES)
+connection = gspread.authorize(credentials)
 
-women_keywords = ["women"]#, "woman", "girl"]
-tech_keywords = ["tech"]#, "technology", "code", "programming", "programmer"]
-csv_titles = "name, description, host, start_time, end_time, event_type, location, city, country, price, link, tags"
+worksheet = connection.open("Test spreadsheet").worksheet("Sheet4")
 
-ids = set()
-lines = [csv_titles]
-rows = [csv_titles.split(', ')]
+ids = set(worksheet.col_values(14))
+first_empty_row = len(ids) + 1
+print(first_empty_row)
+rows = []
+min_date = datetime.date.today().strftime("%Y-%m-%dT00:00:00")
+max_date = (datetime.date.today() + datetime.timedelta(30)).strftime("%Y-%m-%dT00:00:00")
 
+keywords = []
+with open('keywords.txt') as f:
+    keywords = f.read().split("\n")
+keywords = [k.replace(" ", "+").strip() for k in keywords if k]
 
-for i in women_keywords:
-    for j in tech_keywords:
-        keyword = i + "+" + j
-
+for keyword in keywords:
+    for city in ["san+francisco", "london", "new+york"]:
+        print(keyword)
         response = requests.get(
-            "https://www.eventbriteapi.com/v3/events/search/?expand=organizer,venue,format,category&q=" + keyword,
+            ("https://www.eventbriteapi.com/v3/events/search/"
+                "?location.address={}&expand=organizer,venue,format,category&q={}"
+                "&start_date.range_start={}&start_date.range_end={}").format(
+                city, keyword, min_date, max_date),
             headers = {
                 "Authorization": "Bearer " + keys.EVENTBRITE_OAUTH,
             },
             verify = True,  # Verify SSL certificate
         )
+        if not response: continue
 
         for event in [event for event in response.json()['events'] if event['id'] not in ids
                     and event['venue'] and event['status']=='live']:
-            ids.add(event['id']) #TODO: only do updates
+            ids.add(event['id']) #TODO: only do updates but keep tag overlaps!
 
             name = event['name']['text']
-            description = ''#TODO: clean this event['description']['text'] if event['description'] else ''
+            description = event['description']['text'] if event['description'] else ''
             host = event['organizer']['name']
             start_time = event['start']['local']
             end_time = event['end']['local']
@@ -42,28 +56,22 @@ for i in women_keywords:
             price = 'free' if event['is_free'] else 'paid' #TODO: get a general idea of ticket prices
             link = event['url']
             tags = event['category']['name'] if event['category'] else ''
+            source = 'eventbrite'
+            event_id = event['id']
+            keyword = keyword
 
-            details = [name, description, host, start_time, end_time, event_type, location, city, country, price, link, tags]
+            details = [name, description, host, start_time, end_time,
+                event_type, location, city, country, price, link, tags, source, event_id, keyword]
 
-            #CSV formatting
-            line = [str(item).replace(',', ' ') for item in details]
-            line = ", ".join(line)
-            lines.append(line)
-
-            #sheets formatting
             rows.append(details)
 
-#CSV
-with open("events.csv", 'w') as f:
-    f.write('\n'.join(lines))
-
-#sheets
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(keys.GOOGLE_SHEETS, SCOPES)
-connection = gspread.authorize(credentials)
-
-worksheet = connection.open("Test spreadsheet").sheet1
-
+#TODO: error handling for not enough rows
 for row_i in range(len(rows)):
+    print(row_i, "of",len(rows))
     for cell_i in range(len(rows[0])):
         if rows[row_i][cell_i]:
-            worksheet.update_cell(row_i + 1, cell_i + 1, str(rows[row_i][cell_i]))
+            try:
+                worksheet.update_cell(row_i + first_empty_row, cell_i + 1, str(rows[row_i][cell_i]).replace('\n', " "))
+            except TypeError as e:
+                print(e)
+                continue
